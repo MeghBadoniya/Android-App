e69a0623b78fb8c60c90993a62845cde85dbd6cc
Megh Badoniya <badoniyamegh@gmail.com>
2025-04-08 16:53:02 +0530

    Change in UI

--- app/src/main/java/megh/android/MainActivity.kt
+++ app/src/main/java/megh/android/MainActivity.kt
@@ -1,5 +1,6 @@
 package megh.android
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,34 +15,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import megh.android.ui.theme.AndroidAppTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AndroidAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Text("Hello World!!")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidAppTheme {
-        Greeting("Android")
     }
 }
